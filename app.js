421d783a5a7035958f81017ef7f5ae002141f0e7
David Alonso <david@iMac-de-David.local>
2024-08-15 20:18:25 +0200

    agrego proyecto

---
+++ app.js
@@ -0,0 +1,21 @@
+let formulario = document.getElementById ('formulario');
+
+formulario.addEventListener ('submit', function (e){
+    e.preventDefault();
+    console.log ('Has hecho click !CAbrón !!')
+
+    let datos = new FormData (formulario);
+    console.log (datos)
+    console.log (datos.get('usuario'))
+    console.log (datos.get('pass'))
+
+    fetch ('post.php', {
+        method: 'POST',
+        body: datos
+
+    })
+   .then ( res => res.json ())
+   .then ( data => {
+    console.log (data)
+   })
+})
---
+++ post.php
@@ -0,0 +1,11 @@
+<?php
+
+$usuario = $_POST['usuario'];
+$pass = $_POST['pass'];
+
+if ($usuario === '' || $pass=== ''){
+    echo json_encode ('Llena todos los campos');
+
+}else{
+    echo json_encode('Correcto:  Usuario:' .$usuario. ' Pass:'.$pass.);
+}
